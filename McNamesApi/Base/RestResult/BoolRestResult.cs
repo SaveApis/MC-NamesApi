9008adea09fdf76ce9fca6b84ebc5d4fcd8d4fa0
SaveFeelix <felixhake@web.de>
2022-09-26 11:07:44 +0200

    Rename Folder McNamesApi -> RestApi

--- McNamesApi/Base/RestResult/BoolRestResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace McNamesApi.Base.RestResult;
-
-public class BoolRestResult : BaseRestResult<bool>
-{
-    public BoolRestResult(bool error, string message, bool result = default) : base(error, message, result)
-    {
-    }
-}
